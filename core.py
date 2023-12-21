fe156a5641b1431d2cd7d879ebce9b64e3f81045
Star-Meow <sss37507617@gmail.com>
2023-12-21 12:23:52 +0800

    .

--- core.py
+++ core.py
@@ -7,6 +7,7 @@ DB_File = "player_db.db"
 app = Flask(__name__)
 
 frame_counter = 0
+field_ct = 0
 data_cache = None
 
 @app.route('/')
@@ -31,50 +32,12 @@ def use():
 
 @app.route('/data')  #直接確認json
 def data():
-    number = []
-    has_ball = []
-    x_coor = []
-    y_coor = []
-    frame = []
-    data = []
-    conn = sqlite3.connect(DB_File)
-    c = conn.cursor()
-    cursor =c.execute("SELECT round(number),round(has_ball),round(x_coor),round(y_coor),substr(Frame,1,16)FROM players ORDER BY Frame DESC LIMIT 10")
-    result = cursor.fetchall()
-    for result in result:
-        data.append(result)
-    
-    conn.commit()
-    cursor.close()
-    conn.close()
-    for i in range(10):
-        try:
-            number.append(int(data[i][0]))
-        except:
-            number.append("none")
-        try:
-            has_ball.append(int(data[i][1]))
-        except:
-            has_ball.append("none")
-        try:
-            x_coor.append(int(data[i][2]))
-        except:
-            x_coor.append("none")
-        try:
-            y_coor.append(int(data[i][3]))
-        except:
-            y_coor.append("none")
-        try:
-            frame.append(str(data[i][4]))
-        except:
-            frame.append("none")
-    
-    ans = {'number':number,"has_ball":has_ball,"x_coor":x_coor,"y_coor":y_coor,"frame":frame}
-    
-    return jsonify(ans)
-
-
-
+    global field_ct
+    field_ct += 1
+    if field_ct > 6:
+        field_ct = 0
+    fieldans = {'pic_ct': field_ct}
+    return jsonify(fieldans)
 
 @app.route('/data2')
 def data2():
@@ -87,6 +50,8 @@ def data2():
            "y_coor": data[3], "frame": data[4]}
     print(frame_counter)
     return jsonify(ans)
+
+
 
 @app.route('/check')  #確認ajax有沒有抓到/data的json
 def check():
